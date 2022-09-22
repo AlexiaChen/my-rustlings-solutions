fe8280b68c308ba6a7324ce1973358d18cfde74b
MathxH Chen <brainfvck@foxmail.com>
2022-09-22 11:04:15 +0800

    add solutions for strings and collection

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,7 +12,6 @@
 // Execute the command `rustlings hint hashmap2` if you need
 // hints.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -23,6 +22,10 @@ enum Fruit {
     Mango,
     Lychee,
     Pineapple,
+    Orange,
+    Strawberry,
+    Lemon,
+    Watermelon,
 }
 
 fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
@@ -32,12 +35,19 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Mango,
         Fruit::Lychee,
         Fruit::Pineapple,
+        Fruit::Orange,
+        Fruit::Strawberry,
+        Fruit::Lemon,
+        Fruit::Watermelon,
     ];
 
     for fruit in fruit_kinds {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        if !basket.contains_key(&fruit) {
+            basket.insert(fruit, 3);
+        }
     }
 }
 
