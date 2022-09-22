18a37b374700816353335a89e5ddb60ec9ab7c03
MathxH Chen <brainfvck@foxmail.com>
2022-09-22 10:38:27 +0800

    update my solutions

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,10 +8,17 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_apple_price {
+fn calculate_apple_price(num: i32) -> i32 {
+    const one_apple_price_less_40: i32 = 2;
+    const one_apple_price_more_40: i32 = 1;
+    if num <= 40 {
+        one_apple_price_less_40*num
+    } else {
+        one_apple_price_more_40*num
+    }
+}
 
 // Don't modify this function!
 #[test]
--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -1,7 +1,6 @@
 // structs2.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Order {
@@ -26,6 +25,18 @@ fn create_order_template() -> Order {
     }
 }
 
+fn create_own_order() -> Order {
+    Order {
+        name: String::from("Hacker in Rust"),
+        year: 2019,
+        made_by_phone: false,
+        made_by_mobile: false,
+        made_by_email: true,
+        item_number: 123,
+        count: 1,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -34,7 +45,7 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = create_own_order();
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
