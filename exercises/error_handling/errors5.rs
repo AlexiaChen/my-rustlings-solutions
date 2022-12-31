7cd745a08f002df284d8f5a72afd5e698623b277
MathxH Chen <brainfvck@foxmail.com>
2022-12-31 22:35:46 +0800

    Fix error

--- exercises/error_handling/errors5.rs
+++ exercises/error_handling/errors5.rs
@@ -4,16 +4,15 @@
 // It won't compile right now! Why?
 // Execute `rustlings hint errors5` for hints!
 
-// I AM NOT DONE
 
 use std::error;
 use std::fmt;
 use std::num::ParseIntError;
 
 // TODO: update the return type of `main()` to make this compile.
-fn main() -> Result<(), ParseIntError> {
+fn main() -> Result<(), CreationError> {
     let pretend_user_input = "42";
-    let x: i64 = pretend_user_input.parse()?;
+    let x: i64 = pretend_user_input.parse().unwrap();
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
     Ok(())
 }
